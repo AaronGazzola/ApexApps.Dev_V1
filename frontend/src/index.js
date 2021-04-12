f027772d469fda358c13a2b952786b47a088f993
Aaron Gazzola <aaron@apexapps.dev>
2021-04-12 18:25:56 +1000

    IE message

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,30 @@ import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 import 'styles/style.css';
-
-ReactDOM.render(
-	// <React.StrictMode>
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	// </React.StrictMode>,
-	document.getElementById('root')
-);
+if (/*@cc_on!@*/ false || !!document.documentMode) {
+	ReactDOM.render(
+		<p className='ie'>
+			Internet Explorer is not supported by this web app, please use a modern
+			browser such as{' '}
+			<a href='https://www.google.com/chrome/' target='_blank' rel='noreferrer'>
+				Google Chrome
+			</a>{' '}
+			or{' '}
+			<a
+				href='https://www.mozilla.org/en-US/firefox/download/'
+				target='_blank'
+				rel='noreferrer'
+			>
+				Firefox
+			</a>
+		</p>,
+		document.getElementById('root')
+	);
+} else {
+	ReactDOM.render(
+		<Provider store={store}>
+			<App />
+		</Provider>,
+		document.getElementById('root')
+	);
+}
